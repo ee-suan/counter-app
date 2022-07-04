958ea6e8a64e01d69ca3cb43a2d40d3636fe3361
ee suan <eesuan.234@gmail.com>
2022-07-05 01:11:55 +0800

    Bug fixes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,10 +5,7 @@ const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <div className="container-fluid">
-        <a
-          className="navbar-brand"
-          href="https://ee-suan.github.io/counter-app/"
-        >
+        <a className="navbar-brand" href="#">
           Cart{" "}
           <span className="badge rounded-pill bg-secondary m-2">
             {totalCounters}
