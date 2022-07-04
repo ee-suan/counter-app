b2f62445258ee22080427ef3f43b0fcf8f322fe6
ee suan <eesuan.234@gmail.com>
2022-07-05 00:55:18 +0800

    Add refresh to nav bar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,10 @@ const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+        <a
+          className="navbar-brand"
+          href="https://ee-suan.github.io/counter-app/"
+        >
           Cart{" "}
           <span className="badge rounded-pill bg-secondary m-2">
             {totalCounters}
