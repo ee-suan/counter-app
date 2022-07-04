ed22d99124ec9376dea040d32e16c437667d208b
ee suan <eesuan.234@gmail.com>
2022-07-04 23:50:30 +0800

    Updates

---
+++ precache-manifest.6a60f180b549c23e87a801570102d9be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "800fbe8695ee04bd78b5",
+    "url": "/counter-app/static/css/main.b8a7f97c.chunk.css"
+  },
+  {
+    "revision": "800fbe8695ee04bd78b5",
+    "url": "/counter-app/static/js/main.800fbe86.chunk.js"
+  },
+  {
+    "revision": "7f8c91ddc4b58ecb5b87",
+    "url": "/counter-app/static/js/1.7f8c91dd.chunk.js"
+  },
+  {
+    "revision": "f561ed75c7c35ff5e49a",
+    "url": "/counter-app/static/css/2.a6d24774.chunk.css"
+  },
+  {
+    "revision": "f561ed75c7c35ff5e49a",
+    "url": "/counter-app/static/js/2.f561ed75.chunk.js"
+  },
+  {
+    "revision": "44d1cc4428b83b76dc30",
+    "url": "/counter-app/static/js/runtime~main.44d1cc44.js"
+  },
+  {
+    "revision": "6bccecd249c85571bb9bc526506be15d",
+    "url": "/counter-app/index.html"
+  }
+];
